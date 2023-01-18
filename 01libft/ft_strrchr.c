db5824eb42519517ff35f0a6bb50703fc3e63131
Arrate <119618728+arblanck@users.noreply.github.com>
2023-01-18 18:23:36 +0100

    Add files via upload
    ft_strchr.c
    ft_strrchr.c

---
+++ 01libft/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arblanco <arblanco@student.42urduli>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 17:40:15 by arblanco          #+#    #+#             */
+/*   Updated: 2023/01/18 18:16:39 by arblanco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	while (len >= 0)
+	{
+		if ((char)c == str[len])
+			return ((char *)&str[len]);
+		len--;
+	}
+	return (0);
+}
