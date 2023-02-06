0ee98a0d467ae3ae47574958a1596133f99d3c08
Arrate Blanco Careaga <arblanco@c4r8s6.42urduliz.com>
2023-02-06 19:48:50 +0100

    ft_substr pero mal, no pasa la ultima prueba

---
+++ 01libft/ft_substr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arblanco <arblanco@student.42urduli>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 18:42:51 by arblanco          #+#    #+#             */
+/*   Updated: 2023/02/06 19:46:17 by arblanco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*s2;
+	size_t	s_len;
+	size_t	end;
+
+	s_len = ft_strlen(s);
+	end = 0;
+	if (start < s_len)
+		end = s_len - start;
+	if (end > len)
+		end = len;
+	s2 = (char *)malloc(sizeof(char) * (end + 1));
+	if (s == 0 || s2 == 0)
+		return (0);
+	ft_strlcpy(s2, s + start, end + 1);
+	return (s2);
+}
